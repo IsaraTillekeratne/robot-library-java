edcbff5cd5b403122f265a4c9c0105933352eace
IsaraTillekeratne <e17352@eng.pdn.ac.lk>
2023-09-04 00:03:44 +0530

    python script to get graphs

--- src/main/java/Robots/DynamicTaskAllocationRobot.java
+++ src/main/java/Robots/DynamicTaskAllocationRobot.java
@@ -2,6 +2,7 @@ package Robots;
 
 import swarm.behaviours.atomicBehaviours.AtomicBehaviours;
 import swarm.behaviours.clusterBehaviours.ClusterBehaviours;
+import swarm.mqtt.MqttMsg;
 import swarm.robot.exception.SensorException;
 import swarm.robot.types.RGBColorType;
 
@@ -30,14 +31,15 @@ public class DynamicTaskAllocationRobot extends ObstacleAvoidanceRobot{
     float taskSelectionProbabilityRed;
     float taskSelectionProbabilityBlue;
     String selectedTask;
-    int i = 0;
+    int timeStep = 0;
 
-    long startTime = System.currentTimeMillis();
+    long startTime;
     int robotId;
 
-    public DynamicTaskAllocationRobot(int id, double x, double y, double heading) {
+    public DynamicTaskAllocationRobot(int id, double x, double y, double heading, long time) {
         super(id, x, y, heading);
         robotId = id;
+        this.startTime = time;
     }
 
     public void setup() {
@@ -46,7 +48,6 @@ public class DynamicTaskAllocationRobot extends ObstacleAvoidanceRobot{
 
         // initially assign task Red
         selectedTask = "r";
-//        showSelectedTask();
         atomicBehaviours.showSelectedTask(selectedTask,this.neoPixel,this.simpleComm,this.getId());
 
         // assign initial random thresholds
@@ -54,6 +55,8 @@ public class DynamicTaskAllocationRobot extends ObstacleAvoidanceRobot{
 
         this.responseThresholdRed = rand.nextFloat();
         this.responseThresholdBlue = rand.nextFloat();
+
+        CsvRecorder.recordInitialThresholdValues("src/main/java/csvRecorder/record.csv",this.getId(),this.responseThresholdRed,this.responseThresholdBlue);
 
         System.out.println("Robot "+this.id+" is running Dynamic Task Allocation Algorithm");
         System.out.println("Robot initially set to task: "+ selectedTask);
@@ -64,7 +67,6 @@ public class DynamicTaskAllocationRobot extends ObstacleAvoidanceRobot{
     public void loop() throws Exception {
         super.loop();
 //        runTaskAllocationAlgorithm();
-
         action2Future = executor.submit(() -> {
 
             try {
@@ -77,44 +79,27 @@ public class DynamicTaskAllocationRobot extends ObstacleAvoidanceRobot{
             }
 
         });
-        // Wait for both actions to complete
-//        action1Future.get();
-//        action2Future.get();
 
     }
 
-    public void runTaskAllocationAlgorithm() throws SensorException {
+    public synchronized void runTaskAllocationAlgorithm() throws SensorException {
 
         // REAL ALGORITHM STARTS HERE
         if (state == robotState.RUN){
 
-//            observe();
             RGBColorType detectedColor = colorSensor.getColor();
-
-//            System.out.print("Robot: "+this.getId()+" | task demand before observe: ");
-//            printQueue(taskDemandQueue);
 
             clusterBehaviours.observe(detectedColor, this.taskDemandQueue, this.taskSupplyQueue, fixedQueueLength, this.getId(), this.robotMqttClient);
 
-//            System.out.print("Robot: "+this.getId()+" | task demand after observe: ");
-//            printQueue(taskDemandQueue);
-
-//            evaluateTaskDemand();
-//            System.out.println("Robot: "+this.getId()+" "+"Task demand for blue before: "+ estimatedTaskDemandForRed);
             float[] taskDemands = clusterBehaviours.evaluateTaskDemand(this.taskDemandQueue, fixedQueueLength, this.getId());
             this.estimatedTaskDemandForRed = taskDemands[0];
             this.estimatedTaskDemandForBlue = taskDemands[1];
-//            System.out.println("Robot: "+this.getId()+" "+"Task demand for blue after: "+ estimatedTaskDemandForRed);
-//            evaluateTaskSupply();
+
             float[] taskSupplies = clusterBehaviours.evaluateTaskSupply(this.taskSupplyQueue, fixedQueueLength, this.getId());
             this.estimatedTaskSupplyForRed = taskSupplies[0];
             this.estimatedTaskSupplyForBlue = taskSupplies[1];
 
-//            System.out.println("Robot: "+this.getId()+" Selected task: "+selectedTask+" Current Red Threshold: "
-//                    +this.responseThresholdRed+" Current Blue Threshold: "+this.responseThresholdBlue
-//                    +" Red Probability: "+this.taskSelectionProbabilityRed+" Blue Probability: "+this.taskSelectionProbabilityBlue);
 
-            // selectTask();
             List<Object> outputs = clusterBehaviours.selectTask(this.responseThresholdRed,this.responseThresholdBlue,scalingFactor,this.estimatedTaskDemandForRed,this.estimatedTaskSupplyForRed
             ,this.estimatedTaskDemandForBlue,this.estimatedTaskSupplyForBlue,n,this.getId());
 
@@ -124,40 +109,34 @@ public class DynamicTaskAllocationRobot extends ObstacleAvoidanceRobot{
             this.taskSelectionProbabilityRed = (float) outputs.get(3);
             this.taskSelectionProbabilityBlue = (float) outputs.get(4);
 
-//            System.out.println("Robot: "+this.getId()+" Selected task: "+selectedTask+" Next Red Threshold: "
-//            +this.responseThresholdRedNext+" Next Blue Threshold: "+this.responseThresholdBlueNext
-//            +" Red Probability: "+this.taskSelectionProbabilityRed+" Blue Probability: "+this.taskSelectionProbabilityBlue);
 
-            // long endTime = System.currentTimeMillis(); // Record the end time
-            // long elapsedTime = endTime - startTime; // Calculate the elapsed time in milliseconds
-            // double[] values = {
-            //     this.robotId, 
-            //     elapsedTime,
-            //     this.responseThresholdRed,
-            //     this.responseThresholdBlue,
-            //     this.estimatedTaskDemandForRed,
-            //     this.estimatedTaskDemandForBlue,
-            //     this.estimatedTaskSupplyForRed,
-            //     this.estimatedTaskSupplyForBlue,
-            //     this.taskSelectionProbabilityRed,
-            //     this.taskSelectionProbabilityBlue,
-            // };
+            this.timeStep = this.timeStep + 1;
+             long endTime = System.currentTimeMillis(); // Record the end time
+             long elapsedTime = endTime - startTime; // Calculate the elapsed time in milliseconds
+             double[] values = {
+                 this.robotId,
+                 elapsedTime,
+//                     this.timeStep,
+                 this.responseThresholdRed,
+                 this.responseThresholdBlue,
+                 this.estimatedTaskDemandForRed,
+                 this.estimatedTaskDemandForBlue,
+                 this.estimatedTaskSupplyForRed,
+                 this.estimatedTaskSupplyForBlue,
+                 this.taskSelectionProbabilityRed,
+                 this.taskSelectionProbabilityBlue,
+             };
 
-            // CsvRecorder.writeRecordToCSV("src/main/java/csvRecorder/record.csv", values, selectedTask);            
+             CsvRecorder.writeRecordToCSV("src/main/java/csvRecorder/record.csv", values, selectedTask);
 
             this.responseThresholdRed = this.responseThresholdRedNext;
             this.responseThresholdBlue = this.responseThresholdBlueNext;
 
-//            showSelectedTask();
             atomicBehaviours.showSelectedTask(selectedTask,this.neoPixel,this.simpleComm,this.getId());
             delay(2000); // time interval
 
             
         }
     }
-
-//    public void communicationInterrupt(String msg) {
-//        System.out.println("communicationInterrupt on " + id + " with msg:" + msg);
-//    }
 
 }
---
+++ src/main/java/csvRecorder/graphs.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the Excel file into a DataFrame
+csv_file = "record2.csv"  # Replace with the actual file path
+df = pd.read_csv(csv_file)
+
+# Initialize a dictionary to store the tasks assigned to each robot
+robot_tasks = ["r"] * len(df['RobotID'].unique())
+print(robot_tasks)
+
+# Initialize lists to store the time points and proportions of robots with tasks "r" and "b"
+time_points = []
+proportions_r = []
+proportions_b = []
+
+# Iterate through each row of the DataFrame
+for _, row in df.iterrows():
+    robot_id = row['RobotID']
+    time_point = row['Time']
+    task = row['Task']
+
+    # Update the task for the corresponding robot
+    robot_tasks[robot_id] = task
+
+    # Calculate the proportion of robots with tasks "r" and "b" at the current time
+    count_r = robot_tasks.count('r')
+    count_b = robot_tasks.count('b')
+    total_robots = len(robot_tasks)
+    print(robot_tasks)
+
+    proportion_r = count_r / total_robots
+    proportion_b = count_b / total_robots
+
+    # Append the time point and proportions to the lists
+    time_points.append(time_point)
+    proportions_r.append(proportion_r)
+    proportions_b.append(proportion_b)
+
+# Create a single figure for both Time vs Proportion of Robots Assigned to Task "r" and "b"
+plt.figure(figsize=(10, 6))
+
+# Plot Time vs Proportion of Robots Assigned to Task "r"
+plt.plot(time_points, proportions_r, label='Task "r"', color='red')
+
+# Plot Time vs Proportion of Robots Assigned to Task "b"
+plt.plot(time_points, proportions_b, label='Task "b"', color='blue')
+
+plt.xlabel('Time')
+plt.ylabel('Proportion of Robots')
+plt.title('Time vs Proportion of Robots Assigned to Tasks "r" and "b"')
+plt.legend()
+
+# Save or display the combined graph
+plt.savefig("sample_proportion_graph.png")
+plt.show()
+
+
+
+
+
+

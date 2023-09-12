4d720d268af42d53cee816a4f8aa206cca51cf66
Kavinaya12 <e17159@eng.pdn.ac.lk>
2023-09-12 11:27:03 +0530

    reconnection issue

--- src/main/java/swarm/behaviours/atomicBehaviours/AtomicBehaviours.java
+++ src/main/java/swarm/behaviours/atomicBehaviours/AtomicBehaviours.java
@@ -36,4 +36,60 @@ public class AtomicBehaviours {
         System.out.println();
     }
 
+    public static void moveSquiggly(MotionController motion, int speed, int duration) {
+    // Define variables for left and right wheel speeds
+    int leftSpeed = speed;
+    int rightSpeed = speed;
+
+    // Define a variable for the angle change amount
+    int angleChange = 50; // Adjust this value to control the squiggly behavior
+
+    // Loop for half of the specified duration
+    long startTime = System.currentTimeMillis();
+    while (System.currentTimeMillis() - startTime < duration / 2) {
+        // Alternate the wheel speeds to create a squiggly motion
+        motion.move(leftSpeed, rightSpeed, 1000); // Adjust the duration as needed
+
+        // Add an angle change to one of the wheels to make it squiggly
+        leftSpeed += angleChange;
+        // Ensure the wheel speeds stay within a reasonable range
+        leftSpeed = Math.max(0, Math.min(100, leftSpeed)); // Assuming speed range is 0-100
+
+        // Stop the other wheel to make a half circle
+        rightSpeed = 0;
+    }
+
+    // Swap the direction by reversing the angle change
+    angleChange = -angleChange;
+
+    // Loop for the second half of the specified duration
+    startTime = System.currentTimeMillis();
+    while (System.currentTimeMillis() - startTime < duration / 2) {
+        // Alternate the wheel speeds to create a squiggly motion
+        motion.move(leftSpeed, rightSpeed, 100); // Adjust the duration as needed
+
+        // Add an angle change to one of the wheels to make it squiggly
+        leftSpeed += angleChange;
+        // Ensure the wheel speeds stay within a reasonable range
+        leftSpeed = Math.max(0, Math.min(100, leftSpeed)); // Assuming speed range is 0-100
+
+        // Stop the other wheel to complete the circle in the opposite direction
+        rightSpeed = 0;
+    }
+
+    // Stop the motion after completing the full half circle in each direction
+    motion.move(0, 0, 100);
+}
+
+public static void moveHalfCircle(MotionController motion, double speed, int duration) {
+    // Calculate the speeds for turning in a half-circle
+    double leftSpeed = speed;
+    double rightSpeed = -speed;  // Negative speed to make a left turn
+
+   
+        motion.move((int)leftSpeed, (int)rightSpeed, 200); // Adjust the duration as needed
+  
+    motion.move(200, 200, 200);
+}
+
 }
--- src/main/java/swarm/ota/otaUpdate.java
+++ src/main/java/swarm/ota/otaUpdate.java
@@ -1,12 +1,66 @@
 package swarm.ota;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class otaUpdate {
 
-    public void downloadBinFile(){
+    /*public void downloadBinFile(){
         System.out.println("Launching the application in the command prompt...");
         launchApplication();
+    }*/
+ 
+    public void downloadBinFile() {
+        try {
+            String updateJarEndpoint = "http://localhost:5001/updateJar"; 
+
+            // Create a URL object
+            URL url = new URL(updateJarEndpoint);
+
+            // Open a connection to the URL
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Set the request method to GET
+            connection.setRequestMethod("GET");
+
+            // Get the input stream from the connection
+            InputStream inputStream = connection.getInputStream();
+
+            String jarFilePath = "recent_builds/java-robot-1.0.2.jar";
+
+            // Create a File object for the target file
+            File jarFile = new File(jarFilePath);
+
+            // Create parent directories if they don't exist
+        if (!jarFile.getParentFile().exists()) {
+            jarFile.getParentFile().mkdirs();
+        } 
+
+            // Create a FileOutputStream to write the .jar file
+            FileOutputStream outputStream = new FileOutputStream(jarFilePath);
+
+            // Read data from the input stream and write it to the output stream
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            // Close the output stream and input stream
+            outputStream.close();
+            inputStream.close();
+
+            // Print a success message
+            System.out.println("Downloaded .jar file successfully to: " + jarFilePath);
+
+            // Launch the application
+            launchApplication(jarFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Failed to download .jar file.");
+        }
     }
 
     private static void shutdownApplication() {
@@ -15,7 +69,7 @@ public class otaUpdate {
         System.exit(0);
     }
     
-    private static void launchApplication() {
+    private static void launchApplication(String jarFilePath) {
         try {
             String[] command = {"cmd", "/c", "start", "cmd.exe", "/K", "java -cp java-robot-1.0.2.jar @C:\\Users\\USER\\AppData\\Local\\Temp\\cp_39mik0g6gb00qcc5u259cwthi.argfile swarm.App"};
             Process process = new ProcessBuilder(command).inheritIO().start();
